94730d3b9f9a1758e1a5a85941c14a14b6e0691d
Leonardo Ultramari <leonardo.ultramari@aluno.faculdadeimpacta.com.br>
2021-05-08 17:22:03 +0000

    exemplos Terraform

---
+++ terraform/exemplo-data-output/main.tf
@@ -0,0 +1,13 @@
+data "google_compute_image" "jenkins_image" {
+  family  = "ubuntu-minimal-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+output "gcp_image" {
+ value = data.google_compute_image.jenkins_image
+}
+
+output "senha" {
+  value = "minhasenha"
+  sensitive = true
+}
