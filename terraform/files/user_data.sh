e9e3aa5e197497902d563c40569e3074ab642c94
Leonardo Ultramari <leonardo.ultramari@aluno.faculdadeimpacta.com.br>
2021-05-08 17:24:00 +0000

    limpeza

--- terraform/files/user_data.sh
+++
@@ -1,5 +0,0 @@
-apt update
-apt install -y nginx
-systemctl start nginx
-systemct enable nginx
-echo "<h1> Impacta Turma 04 - Infrastructure as a Code</h1>" > /var/www/html/index.nginx-debian.html
--- terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  project = "leonardo-mbaimpacta-turma04"
-  region = "us-central1"
-  zone = "us-central1-a"
-  credentials = "../../leonardo-mbaimpacta-turma04.json"
-}
